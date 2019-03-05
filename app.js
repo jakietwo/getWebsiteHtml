75f264b57e9478d8830b7c8e00926fa54635ce8e
jakietwo <823266678@qq.com>
2019-03-05 23:46:18 +0800

    尝试

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var superagent = require('superagent')
+var charset = require('superagent-charset')
+charset(superagent)
+var baseurl = 'https://www.csis.org/search?search_api_views_fulltext=china&sort_by=search_api_relevance'
+const cheerio = require('cheerio')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,14 +27,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/index', function (req, res) {
+  // 设置请求头
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  
 
+})
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
